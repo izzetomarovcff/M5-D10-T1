13ad0c6ebd4d766f7f489dd6401d8208c20a0d41
izzetomarovcff <izzetomarovcff@gmail.com>
2022-12-20 12:15:40 +0400

    end

---
+++ data.sql
@@ -0,0 +1,12 @@
+-- DROP DATABASE task
+CREATE DATABASE task;
+USE task;
+CREATE TABLE tasks (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  description VARCHAR(200) ,
+  due_date DATE ,
+  employee VARCHAR(100) ,
+	finished_date DATE 
+);
+
+-- data imported on excel
